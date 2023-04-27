ce9b15c41d9dc7a308315384318772507bad44c4
amarjeet singh <amarjeetsingh94620@gmail.com>
2023-04-27 16:44:32 +0530

    first commit

--- app.js
+++ app.js
@@ -8,14 +8,16 @@ const {createProxyMiddleware} = require('http-proxy-middleware');
 
 
 const app = express();
+
 app.use(cors());
-app.use(function(req, res, next) {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-    res.setHeader('Access-Control-Allow-Credentials', true);
-    next();
-});
+
+// app.use(function(req, res, next) {
+//     res.setHeader('Access-Control-Allow-Origin', '*');
+//     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+//     res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+//     res.setHeader('Access-Control-Allow-Credentials', true);
+//     next();
+// });
 app.use(bodyparser.urlencoded({extended:false}))
 app.use(bodyparser.json())
  
