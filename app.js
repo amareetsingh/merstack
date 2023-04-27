084b0d5532790ffc4f4390e0b8f8e17fa9bb8fb5
amarjeet singh <amarjeetsingh94620@gmail.com>
2023-04-27 15:48:02 +0530

    new corrs error fix

--- app.js
+++ app.js
@@ -6,18 +6,14 @@ const bodyparser = require('body-parser');
 const cors = require('cors');
 const {createProxyMiddleware} = require('http-proxy-middleware');
 
-const app = express();
-
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    next();
-  });
-
-app.use(
-    cors()
-)
-app.use(bodyparser.urlencoded({extended:false}))
-app.use(bodyparser.json())
+app.use(cors());
+app.use(function(req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  next();
+});
  
 dotenv.config({path:'./config.env'})
 const port = process.env.PORT || 5000;
